331272e394479ae59dba505662fd2e02e9c48441
Giovanni Gemme 5452954 <115399060+S5452954@users.noreply.github.com>
2022-12-13 21:02:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include "esercitazione_9.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+int main() {
+
+    char elenco [10][20];
+    char nome [20];
+
+    for (int i=0; i<10; i++) {
+        cin >> elenco[i];
+    }
+
+    cin >> nome;
+
+    int a = funzione(elenco, nome);
+    if (a <= 10) {
+        cout << a;
+    }
+    else {
+        cout << "non presente";
+    }
+
+    return 0;
 }
