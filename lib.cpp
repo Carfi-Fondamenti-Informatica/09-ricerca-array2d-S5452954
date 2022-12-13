136401a4ffafa60a234e2434175fb12b69ed2e10
Giovanni Gemme 5452954 <115399060+S5452954@users.noreply.github.com>
2022-12-13 21:02:08 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "esercitazione_9.h"
+#include "cstring"
 
+int funzione (char elenco [10][20], char nome[20]) {
+
+    for (int i = 0; i < 10; i++) {
+        if (strcmp(elenco [i], nome) == 0) {
+            return i;
+        }
+    }
+
+    return 44;
+}
